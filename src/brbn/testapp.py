f379134a317b7f574362618892675bfe2982cddd
Justin Ross <jross@apache.org>
2022-08-20 06:12:43 -0400

    WIP

--- src/brbn/testapp.py
+++ src/brbn/testapp.py
@@ -1,3 +1,22 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
 from brbn import *
 from brbn.plano import *
 
@@ -20,18 +39,14 @@ class Json(Resource):
     async def process(self, request):
         data = await request.parse_json()
 
-class PostOnly(Resource):
-    def __init__(self):
-        super().__init__(method="POST")
-
 class RequiredParam(Resource):
     async def process(self, request):
-        request.require("not-there")
+        not_there = request.require("not-there")
 
 server.add_route("/", Main())
 server.add_route("/explode", Explode())
 server.add_route("/files/alpha.txt", PinnedFileResource(join(static_dir, "alpha.txt")))
 server.add_route("/files/*", StaticDirectoryResource(static_dir))
 server.add_route("/json", Json())
-server.add_route("/post-only", PostOnly())
+server.add_route("/post-only", Resource(method="POST"))
 server.add_route("/required-param", RequiredParam())
